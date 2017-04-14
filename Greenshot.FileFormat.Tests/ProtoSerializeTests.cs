e54efca3338dcb162c80eaa11e257c2af7bbda44
Robin <robin@dapplo.net>
2017-04-14 22:11:39 +0200

    First type for a new file format.

---
+++ Greenshot.FileFormat.Tests/ProtoSerializeTests.cs
@@ -0,0 +1,21 @@
+﻿using Google.Protobuf;
+using Greenshot.FileFormat.Proto;
+using System.IO;
+using Xunit;
+
+namespace Greenshot.FileFormat.Tests
+{
+    public class ProtoSerializeTests
+    {
+        [Fact]
+        public void TestSerialize()
+        {
+            var screenshot = new GreenshotFile();
+
+            using (var output = File.Create("screenshot.gs"))
+            {
+                screenshot.WriteTo(output);
+            }
+        }
+    }
+}
